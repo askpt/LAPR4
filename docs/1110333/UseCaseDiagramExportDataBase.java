1bd5d901de91780f949a7cd8ccd78ec5dd3d49af
Tiago José Leal Pacheco <tiagolealpacheco@gmail.com>
2013-05-29 17:40:56 +0100

    #5 Added use case diagram for export to database

---
+++ docs/1110333/UseCaseDiagramExportDataBase.java
@@ -0,0 +1,18 @@
+/*
+
+@startuml
+
+left to right direction
+
+title  <b>UseCaseDiagram Export to DataBase
+
+User -> (Export to DataBase)
+
+(Export to DataBase) .> (Select data to export) : include
+(Export to DataBase) .> (Choose DataBase) : include
+(Export to DataBase) .> (Insert name of DataBase) : include
+(Export to DataBase) .> (Insert Access Credentials) : include
+
+
+@enduml
+*/
