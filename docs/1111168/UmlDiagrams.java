53baeba73cb1487e1d82ec4e66b88610f23f7b04
jpcarreira <jpmavcarreira@gmail.com>
2013-05-29 23:22:15 +0100

    1111168: UC diagram ref #4 #8

---
+++ docs/1111168/UmlDiagrams.java
@@ -0,0 +1,13 @@
+/**
+ * Week 1 - Adding new functionalites to formulae language
+ */
+/*
+@startuml Diagrams/use_case.png
+
+left to right direction
+
+User --> (inserts expression starting with cardinal) 
+User --> (assigns expression to a cell)
+
+@enduml 
+*/
