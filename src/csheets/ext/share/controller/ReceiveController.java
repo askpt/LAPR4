88f37ebea00db7b61e2b0ea800a0073479f65d55
André Silva <2493377+askpt@users.noreply.github.com>
2013-05-31 12:00:22 +0100

    1100859: Implementation of sequence diagram for client ref #2

---
+++ src/csheets/ext/share/controller/ReceiveController.java
@@ -0,0 +1,30 @@
+package csheets.ext.share.controller;
+
+import csheets.core.Cell;
+import csheets.ext.share.core.Client;
+
+/**
+ * Controller of the receive action
+ * 
+ * @author Andre
+ * 
+ */
+public class ReceiveController {
+
+    /**
+     * Method that will create a new client and will start the connection
+     * 
+     * @param IP
+     *            the server ip
+     * @param port
+     *            the connection port of the server
+     * @param cellStart
+     *            cell where we paste the content of the shared cells
+     * @return the result of the connection
+     */
+    public boolean startClient(String IP, int port, Cell cellStart) {
+	Client cli = new Client();
+	return cli.startClient(IP, port, cellStart);
+    }
+
+}
