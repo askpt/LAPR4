b0cd88e5920b5bd02cb4a80fe7c3b8d49af4158b
André Silva <andresilva15@outlook.com>
2013-06-05 23:15:45 +0100

    1100859: Added JUnit tests for new Language compiler (assignment). Updated Scripts ref #8

--- scripts/run_tests.bat
+++ scripts/run_tests.bat
@@ -1,2 +1,2 @@
-@java -cp ..\tmp-build-tests;..\dist\csheets.jar;..\lib\antlr.jar;..\lib\junit-4.10.jar org.junit.runner.JUnitCore csheets.core.WorkbookTest csheets.core.SpreadsheetTest csheets.ext.comments.CommentableCellTest csheets.core.ext.share.ValidateTest csheets.core.ext.share.SendReceiveTest
+@java -cp ..\tmp-build-tests;..\dist\csheets.jar;..\lib\antlr.jar;..\lib\junit-4.10.jar org.junit.runner.JUnitCore csheets.core.WorkbookTest csheets.core.SpreadsheetTest csheets.ext.comments.CommentableCellTest csheets.core.ext.share.ValidateTest csheets.core.ext.share.SendReceiveTest csheets.core.language.NumberSignCompilerTest
 
