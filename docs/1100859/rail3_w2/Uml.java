2c33f201abbe365e0ce1750b87c94e5b247426a7
André Silva <2493377+askpt@users.noreply.github.com>
2013-06-05 21:26:21 +0100

    1100859: Added sequence diagram to have a sequence of expressions in the cell ref #11

--- docs/1100859/rail3_w2/Uml.java
+++ docs/1100859/rail3_w2/Uml.java
@@ -30,11 +30,76 @@ us -> sys : insert a sequence of expressions
 us -> sys : presses return
 sys -> sys : compiles expression
 sys -> us : display the result of last iteration in the cells
-
-
 @enduml
 
 
+@startuml Diagrams/sd_cell_lang.png
+note left of NumberSignExpressionCompiler
+ to assign a value to a cell
+ we describe method convert() 
+end note
+note left of NumberSignExpressionCompiler
+ this method is recursive
+ therefore it is included
+ in a loop
+end note 
+loop recursive calls depending on the depth of the generated ANTLR AST
+ NumberSignExpressionCompiler -> ASTPair: getNumberOfChildren()
+ alt if numberOfChildren() == 0
+  NumberSignExpressionCompiler -> ASTPair: getType()
+  alt if node == NUMBER
+   NumberSignExpressionCompiler -> Literal: new()
+  else if node == STRING
+   NumberSignExpressionCompiler -> Literal: new()
+  else if node == CELL_REF
+   NumberSignExpressionCompiler -> CellReference: new()
+  end
+   NumberSignExpressionCompiler -> Language: getInstance()
+   NumberSignExpressionCompiler -> Language: getFunction()
+  opt if function != NULL
+   NumberSignExpressionCompiler -> ASTPair: getFirstChild()
+   loop recursive calls while getFirstChild() != NULL
+    NumberSignExpressionCompiler -> NumberSignExpressionCompiler: convert(cell, child)
+   end
+   NumberSignExpressionCompiler -> FunctionCall: new()
+  end
+  else if numberOfChildren == 1
+   NumberSignExpressionCompiler -> Language: getInstance()
+   NumberSignExpressionCompiler -> Language: getUnaryOperator(node.getText())
+   loop recursive calls while node.firstChild() != NULL
+    NumberSignExpressionCompiler -> NumberSignExpressionCompiler: convert(cell, node.firstChild())
+   end
+    NumberSignExpressionCompiler -> UnaryOperation: new()
+  else if numberOfChildren >= 2
+   NumberSignExpressionCompiler -> Language: getInstance()
+   alt node == ';'
+   NumberSignExpressionCompiler -> NumberSignExpressionCompiler : convert(cell, node.getFirstChild())
+   note left 
+   	convert the current child node
+   end note
+   NumberSignExpressionCompiler --> NumberSignExpressionCompiler : convert(cell, node.getFirstChild().getNextSibling())
+   note left 
+   	convert the next child node
+   end note
+   else
+   NumberSignExpressionCompiler -> Language: getBinaryOperator(node.getText())
+   alt operator is Attribution
+    NumberSignExpressionCompiler -> CellReference: new()
+    note left: new code to deal with ':='
+    NumberSignExpressionCompiler -> CellReference: getCell()
+    NumberSignExpressionCompiler -> ASTPair: getFirstChild()
+    NumberSignExpressionCompiler -> NumberSignExpressionCompiler: convert()
+    NumberSignExpressionCompiler -> Expression: value = evaluate()
+    NumberSignExpressionCompiler -> UpdateCellContent: getInstance()
+    NumberSignExpressionCompiler -> UpdateCellContent: triggerUpdate()
+    UpdateCellContent -> Cell: setContent(value)
+   else else
+    NumberSignExpressionCompiler -> NumberSignExpressionCompiler: convert(cell, node.getFirstChild())
+    NumberSignExpressionCompiler -> NumberSignExpressionCompiler: convert(cell, node.getNextSibling()) 
+   end
+ end
+end
+@enduml
 
 */
 
@@ -55,7 +120,10 @@ sys -> us : display the result of last iteration in the cells
 * </p>
 * <img src="doc-files/sd_system_loops.png">
 *
-*
+*<p>
+*<b>Sequence Diagram - Implementation of loops</b>
+*</p>
+*<img src="doc-files/sd_cell_lang.png">
 *
 *
 * @author Andre
