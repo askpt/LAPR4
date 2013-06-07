930d5d884f1d170b65b5d57bfc63505dc4663b79
jpcarreira <jpmavcarreira@gmail.com>
2013-06-07 15:35:46 +0100

    1111168: added prototype for the factory pattern ref #5 #9 #10

---
+++ src/csheets/ext/persistence/adapter/ConsoleTest.java
@@ -0,0 +1,16 @@
+package csheets.ext.persistence.adapter;
+
+
+/**
+ *
+ * @author joaocarreira
+ */
+public class ConsoleTest 
+{
+    public static void main(String args[])
+    {
+       
+        
+        System.out.println(DBConnectionAdapterFactory.getDBTechnology("http:google.com"));
+    }
+}
