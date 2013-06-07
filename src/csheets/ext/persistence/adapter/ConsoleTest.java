34df353b57e9b01db9e91b380df40baef715a820
jpcarreira <jpmavcarreira@gmail.com>
2013-06-07 15:37:35 +0100

    1111168: fix factory pattern ref #5 #9 #10

--- src/csheets/ext/persistence/adapter/ConsoleTest.java
+++ src/csheets/ext/persistence/adapter/ConsoleTest.java
@@ -2,15 +2,13 @@ package csheets.ext.persistence.adapter;
 
 
 /**
- *
+ * A test console 
  * @author joaocarreira
  */
 public class ConsoleTest 
 {
     public static void main(String args[])
     {
-       
-        
-        System.out.println(DBConnectionAdapterFactory.getDBTechnology("http:google.com"));
+    
     }
 }
