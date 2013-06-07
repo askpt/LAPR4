03f436a3a9677e87d841b27aa71c6760e0522118
jpcarreira <jpmavcarreira@gmail.com>
2013-06-07 20:19:58 +0100

    1111168: added facade and observer patterns ref #4 #9 #10

---
+++ src/csheets/ext/database/core/DatabaseFacade.java
@@ -0,0 +1,11 @@
+package csheets.ext.database.core;
+
+/**
+ * A class that deals with all the data going into and from the databases
+ * (UML facade pattern)
+ * @author João Carreira
+ */
+public class DatabaseFacade
+{
+    
+}
--- src/csheets/ext/database/ui/UIExport.java
+++ src/csheets/ext/database/ui/UIExport.java
@@ -1,17 +1,60 @@
 package csheets.ext.database.ui;
 
 import csheets.core.Cell;
+import csheets.ext.database.controller.ControllerExport;
+import java.util.Observable;
+import java.util.Observer;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JTextField;
 
 /**
- *
+ * GUI to export to database 
  * @author João Carreira
  */
-public class UIExport 
+public class UIExport extends JFrame implements Observer
 {
-
-    UIExport(Cell[][] cells) 
+    /* database available drivers stored in a string and displayed in a combobox */
+    private String []dbDrivers;
+    private JComboBox comboDrivers;
+    
+    /* controller object for GUI-controller pattern */
+    private ControllerExport ctrlExp;
+    
+    /* buttons */
+    private JButton btnOK = new JButton("OK");
+    private JButton btnCancel = new JButton("Cancel");
+    
+    /* selected cells to export in a 2D array*/
+    private Cell [][]cells;
+    
+    /* textfields for username, passord and database */
+    private JTextField userTxt, pwdTxt, dbText;
+   
+    /**
+     * Export GUI constructor
+     * @param cells
+     * @throws Exception 
+     */
+    public UIExport(Cell[][] cells) throws Exception
     {
-        throw new UnsupportedOperationException("Not supported yet."); 
+        /* window title */
+        super("Export information to database");
+        
+        /* saving argument of this function is class variable */
+        this.cells = cells;
+        
+        /* creating a new controller */
+        ctrlExp = new ControllerExport(this);
+ 
+        
+    }
+
+    @Override
+    public void update(Observable o, Object arg) 
+    {
+        /* TODO auto-generated code */
     }
     
 }
