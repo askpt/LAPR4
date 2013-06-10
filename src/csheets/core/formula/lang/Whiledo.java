176be71f2b2bc0e3fabae0b7adc2d671722a36aa
André Silva <2493377+askpt@users.noreply.github.com>
2013-06-10 01:19:32 +0100

    1100859: Fixed a bug with whiledo ref #12

--- src/csheets/core/formula/lang/Whiledo.java
+++ src/csheets/core/formula/lang/Whiledo.java
@@ -12,44 +12,44 @@ import csheets.core.formula.*;
  */
 public class Whiledo implements Function {
 
-    /**
-     * Create a new instance of function while do
-     */
-    public Whiledo() {
-    }
-
-    /** Parameters: condition, execution statements */
-    public static final FunctionParameter[] parameters = new FunctionParameter[] { new FunctionParameter(
-	    Value.Type.UNDEFINED, "Iterations", false,
-	    "The iterators of expression.") };
-
-    @Override
-    public String getIdentifier() {
-	return "WHILEDO";
-    }
-
-    @Override
-    public Value applyTo(Expression[] args) throws IllegalValueTypeException {
-	// arg[0] = conditionals
-	// arg[>0] = iterations
-	int i = 1;
-	while (args[0].evaluate().toBoolean()) {
-	    for (; i < (args.length - 1); i++) {
-		args[i].evaluate();
-	    }
-	    return args[i].evaluate();
+	/**
+	 * Create a new instance of function while do
+	 */
+	public Whiledo() {
 	}
-	return args[i + 1].evaluate();
-    }
 
-    @Override
-    public FunctionParameter[] getParameters() {
-	return parameters;
-    }
+	/** Parameters: condition, execution statements */
+	public static final FunctionParameter[] parameters = new FunctionParameter[] { new FunctionParameter(
+			Value.Type.UNDEFINED, "Iterations", false,
+			"The iterators of expression.") };
 
-    @Override
-    public boolean isVarArg() {
-	return true;
-    }
+	@Override
+	public String getIdentifier() {
+		return "WHILEDO";
+	}
+
+	@Override
+	public Value applyTo(Expression[] args) throws IllegalValueTypeException {
+		// arg[0] = conditionals
+		// arg[>0] = iterations
+		int i = 1;
+		while (args[0].evaluate().toBoolean()) {
+			for (; i < (args.length - 1); i++) {
+				args[i].evaluate();
+			}
+			return args[i].evaluate();
+		}
+		return args[i].evaluate();
+	}
+
+	@Override
+	public FunctionParameter[] getParameters() {
+		return parameters;
+	}
+
+	@Override
+	public boolean isVarArg() {
+		return true;
+	}
 
 }
