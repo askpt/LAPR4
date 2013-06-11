649980d6021e54b655bba470ae711dcfdb15a966
jpcarreira <jpmavcarreira@gmail.com>
2013-06-11 19:57:09 +0100

    1111168: GUI for main import concluded ref #9

--- src/csheets/ext/database/ui/UIImport.java
+++ src/csheets/ext/database/ui/UIImport.java
@@ -15,6 +15,8 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Observable;
 import java.util.Observer;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
@@ -31,7 +33,7 @@ import javax.swing.JTextField;
 public class UIImport extends JFrame implements Observer
 {
     /* select cells to import in a 2D array */
-    private Cell [][]cells;
+    private Cell cell;
     
     /* controller object for GUI-controller pattern */
     private ControllerImport ctrlImp;
@@ -40,6 +42,9 @@ public class UIImport extends JFrame implements Observer
     private String [][]dbDrivers;
     private String []driversName;
     private JComboBox comboDrivers;
+    
+    /* main panel */
+    private JPanel mainPanel;
     
     /* buttons */
     private JButton btnOk = new JButton("OK");
@@ -58,13 +63,13 @@ public class UIImport extends JFrame implements Observer
      * @param cells cells 
      * @throws Exception 
      */
-    UIImport(Cell[][] cells) throws Exception
+    public UIImport(Cell cell) throws Exception
     {
         /* window title */
         super("Import information from a database");
         
         /* saving argument of this function is class variable */
-        this.cells = cells;
+        this.cell = cell;
         
         /* creating a new controller */
         ctrlImp = new ControllerImport(this);
@@ -78,12 +83,8 @@ public class UIImport extends JFrame implements Observer
         }
         comboDrivers = new JComboBox(driversName);
         
-        
-        
-        
-        
         /* main panel */
-        JPanel mainPanel = new JPanel(new BorderLayout());
+        mainPanel = new JPanel(new BorderLayout());
         
         /* defining labels */
         JLabel lblDBdrivers = new JLabel("Database");
@@ -92,8 +93,8 @@ public class UIImport extends JFrame implements Observer
         JLabel lblUrl = new JLabel("URL");
         
         /* setting default system message text and color */
-        sysMsg.setText("Connect to a database");
-        sysMsg.setForeground(Color.RED);
+        sysMsg.setText("Fill the required fields");
+        sysMsg.setForeground(Color.BLUE);
         
         /* defining text fields */
         userTxt = new JTextField(30);
@@ -149,11 +150,29 @@ public class UIImport extends JFrame implements Observer
             /* default url button */
             if(e.getSource() == btnUrl)
             {
-               
+                urlTxt.setText(dbDrivers[comboDrivers.getSelectedIndex()][1]);
             }
             /* button OK*/
             else if(e.getSource() == btnOk)
             {
+                /* checks if all fields are filled */
+                if(userTxt.getText().trim().length() == 0
+                        || pwd.getPassword().length == 0
+                        || urlTxt.getText().trim().length() == 0)
+                {
+                    sysMsg.setText("Username/password/url required!");
+                    sysMsg.setForeground(Color.RED);
+                }
+                /* tries to connect if all fields are filled */
+                else
+                {
+                   /* the combo index indicates which database will be used */
+                   int index = comboDrivers.getSelectedIndex();
+                   /* database name */
+                   String dbName = comboDrivers.getSelectedItem().toString();
+                   /* launches the select table window */
+                   TableSelectUI ts = new TableSelectUI(dbName);
+                }
             }
             /* button cancel */
             else if(e.getSource() == btnCancel)
@@ -162,8 +181,7 @@ public class UIImport extends JFrame implements Observer
             }     
         }
     }
-    
-    
+        
     @Override
     public void update(Observable o, Object o1) 
     {
