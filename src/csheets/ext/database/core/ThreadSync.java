3b108254606262da0fd6eca1397f3aa72914f79f
André Silva <andresilva15@outlook.com>
2013-06-12 10:24:29 +0100

    1100859: Added the gui for the sync function ref #15

---
+++ src/csheets/ext/database/core/ThreadSync.java
@@ -0,0 +1,19 @@
+package csheets.ext.database.core;
+
+import csheets.core.Cell;
+import csheets.ext.database.controller.ControllerSync;
+
+public class ThreadSync implements Runnable {
+
+	public ThreadSync(Cell[][] cells, String url, String user, String pass,
+			String table, String dbName, ControllerSync ctrlSync) {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+}
--- src/csheets/ext/database/ui/DatabaseMenu.java
+++ src/csheets/ext/database/ui/DatabaseMenu.java
@@ -8,30 +8,32 @@ import csheets.ui.ctrl.UIController;
 
 /**
  * UI extension menu regarding databases
+ * 
  * @author João Carreira
  */
-public class DatabaseMenu extends JMenu 
-{
-    /**
+public class DatabaseMenu extends JMenu {
+	/**
      * 
      */
-    private static final long serialVersionUID = 1069502879234788510L;
+	private static final long serialVersionUID = 1069502879234788510L;
 
-        /**
-	 * Creates a new simple menu.
-	 * This constructor creates and adds the menu options. 
-	 * In this simple example only one menu option is created.
-	 * A menu option is an action (in this case {@link csheets.ext.simple.ui.ExampleAction})
-	 * @param uiController the user interface controller
+	/**
+	 * Creates a new simple menu. This constructor creates and adds the menu
+	 * options. In this simple example only one menu option is created. A menu
+	 * option is an action (in this case
+	 * {@link csheets.ext.simple.ui.ExampleAction})
+	 * 
+	 * @param uiController
+	 *            the user interface controller
 	 */
-	public DatabaseMenu(UIController uiController) 
-	{
-            super("Database");
-            setMnemonic(KeyEvent.VK_E);
+	public DatabaseMenu(UIController uiController) {
+		super("Database");
+		setMnemonic(KeyEvent.VK_E);
 
-            // menu options
-            add(new Export(uiController));
-            add(new Import(uiController));
-            //add(new Update(uiController));
-	}	
+		// menu options
+		add(new Export(uiController));
+		add(new Import(uiController));
+		// add(new Update(uiController));
+		add(new Sync(uiController));
+	}
 }
