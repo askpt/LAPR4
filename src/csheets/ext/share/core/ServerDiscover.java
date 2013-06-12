e0862c8f121bdbbfddf7eba4ad2d9d8597cd87bb
André Silva <2493377+askpt@users.noreply.github.com>
2013-06-12 15:14:20 +0100

    1100859: Fixed a bug (Only show an active instance of application) ref #14

--- src/csheets/ext/share/core/ServerDiscover.java
+++ src/csheets/ext/share/core/ServerDiscover.java
@@ -10,91 +10,91 @@ import java.net.*;
  * 
  */
 public class ServerDiscover implements Runnable {
-    /** Instance of server discover */
-    private static ServerDiscover instance = null;
-    /** Port of the connection */
-    private int port;
+	/** Instance of server discover */
+	private static ServerDiscover instance = null;
+	/** Port of the connection */
+	private int port;
 
-    /**
-     * Creates a new client discover
-     */
-    private ServerDiscover() {
-    }
-
-    /**
-     * The instance of running server discover
-     * 
-     * @return the instance of the class
-     */
-    public static ServerDiscover getInstance() {
-	if (instance == null) {
-	    instance = new ServerDiscover();
+	/**
+	 * Creates a new client discover
+	 */
+	private ServerDiscover() {
 	}
-	return instance;
-    }
 
-    /**
-     * Broadcasts a message throw network
-     * 
-     * @param port
-     *            the port of the connection
-     * @throws IOException
-     *             signals if the I/O Exception occurs
-     */
-    public void findClients(int port) throws IOException {
-	this.port = port;
-	Thread thr = new Thread(getInstance());
-	thr.start();
-    }
-
-    /**
-     * Broadcasts a message throw network
-     * 
-     */
-    @SuppressWarnings("resource")
-    private void broadcast() {
-	try {
-	    DatagramSocket serverSocket;
-	    serverSocket = new DatagramSocket(9876);
-
-	    byte[] receiveData = new byte[1024];
-	    byte[] sendData = new byte[1024];
-	    int i = 1;
-	    while (i > 0) {
-		DatagramPacket receivePacket = new DatagramPacket(receiveData,
-			receiveData.length);
-
-		serverSocket.receive(receivePacket);
-		String testReceive = new String(receivePacket.getData());
-		testReceive = Validate.removeMessage(testReceive);
-
-		if (testReceive.equals("send me connection")) {
-		    InetAddress IPAddress = receivePacket.getAddress();
-		    int portPacket = receivePacket.getPort();
-
-		    int portSend = port;
-
-		    String portConnect = portSend + "";
-		    int portLenght = portConnect.length();
-		    portConnect = portLenght + "-" + portSend;
-
-		    sendData = portConnect.getBytes();
-		    DatagramPacket sendPacket = new DatagramPacket(sendData,
-			    sendData.length, IPAddress, portPacket);
-		    serverSocket.send(sendPacket);
+	/**
+	 * The instance of running server discover
+	 * 
+	 * @return the instance of the class
+	 */
+	public static ServerDiscover getInstance() {
+		if (instance == null) {
+			instance = new ServerDiscover();
 		}
-	    }
-	    // serverSocket.close();
-
-	} catch (IOException e) {
+		return instance;
 	}
-    }
 
-    /**
-     * Running thread
-     */
-    @Override
-    public void run() {
-	broadcast();
-    }
+	/**
+	 * Broadcasts a message throw network
+	 * 
+	 * @param port
+	 *            the port of the connection
+	 * @throws IOException
+	 *             signals if the I/O Exception occurs
+	 */
+	public void findClients(int port) throws IOException {
+		this.port = port;
+		Thread thr = new Thread(getInstance());
+		thr.start();
+	}
+
+	/**
+	 * Broadcasts a message throw network
+	 * 
+	 */
+	@SuppressWarnings("resource")
+	private void broadcast() {
+		try {
+			MulticastSocket serverSocket;
+			serverSocket = new MulticastSocket(9876);
+
+			byte[] receiveData = new byte[1024];
+			byte[] sendData = new byte[1024];
+			int i = 1;
+			while (i > 0) {
+				DatagramPacket receivePacket = new DatagramPacket(receiveData,
+						receiveData.length);
+
+				serverSocket.receive(receivePacket);
+				String testReceive = new String(receivePacket.getData());
+				testReceive = Validate.removeMessage(testReceive);
+
+				if (testReceive.equals("send me connection")) {
+					InetAddress IPAddress = receivePacket.getAddress();
+					int portPacket = receivePacket.getPort();
+
+					int portSend = port;
+
+					String portConnect = portSend + "";
+					int portLenght = portConnect.length();
+					portConnect = portLenght + "-" + portSend;
+
+					sendData = portConnect.getBytes();
+					DatagramPacket sendPacket = new DatagramPacket(sendData,
+							sendData.length, IPAddress, portPacket);
+					serverSocket.send(sendPacket);
+				}
+			}
+			// serverSocket.close();
+
+		} catch (IOException e) {
+		}
+	}
+
+	/**
+	 * Running thread
+	 */
+	@Override
+	public void run() {
+		broadcast();
+	}
 }
