f0bcc72a3a3380605eb4eba521b528eff3c0498a
jpcarreira <jpmavcarreira@gmail.com>
2013-06-13 22:41:34 +0100

    1111168: fixed import from DB so that it imports to A1 ref #9

--- src/csheets/ext/database/ui/UITableSelect.java
+++ src/csheets/ext/database/ui/UITableSelect.java
@@ -145,7 +145,7 @@ public class UITableSelect extends JFrame
                 try 
                 {
                     /* getting the starting row, which is defined in any of the first columns */
-                    int startRow = Integer.parseInt(tableData[1][0]);
+                    //int startRow = Integer.parseInt(tableData[1][0]);
                     
                     /* cycles the entire tableData array */
                     for(int i = 0; i < tableData.length; i++)
@@ -154,7 +154,8 @@ public class UITableSelect extends JFrame
                         {         
                             /* changes the content of the given cell taking into account the row
                              (we have to subtract 2 to go to right place) */
-                            spreadSheet.getCell(startRow + (j - 2), i).setContent(tableData[i][j]);
+                            //spreadSheet.getCell(startRow + (j - 2), i).setContent(tableData[i][j]);
+                            spreadSheet.getCell(j - 1, i).setContent(tableData[i][j]);
                         }
                     }
                 }
